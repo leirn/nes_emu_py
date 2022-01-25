fbb47b76cedfe854a0efbe9e79b0022947ea5e57
Laurent VROMMAN <laurent@vromman.org>
2022-01-25 14:40:09 +0100

    Linting

--- src/utils.py
+++ src/utils.py
@@ -1,2 +1,10 @@
 def format_hex_data(val):
-	return ' '.join(a+b for a,b in zip(f'{val:x}'[::2], f'{val:x}'[1::2]))
+    """Format hex data with spaces every two hexadecimal characters
+    
+    Arguments
+    val -- array of bytes to convert
+    
+    Returns
+    Formated strings
+    """
+    return ' '.join(a+b for a,b in zip(f'{val:x}'[::2], f'{val:x}'[1::2]))
